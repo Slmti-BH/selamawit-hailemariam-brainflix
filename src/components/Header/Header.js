809f734118b8d926386fb4a207dd0b84d1244177
Selamawit Hailemariam <selamatwits@gmail.com>
2021-12-05 00:47:15 -0800

    Add responsive styles to header section.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,31 +1,28 @@
 import brainFlixLogo from "../../assets/images/logo/BrainFlix-logo.svg";
-// import Image from "../Image/Image";
-import muhanMuruge from "../../assets/images/images/Mohan-muruge.jpg";
-import ProfileImageContainer from  "../ProfileImageContainer/ProfileImageContainer" 
-import "./Header.scss"
+import "./Header.scss";
 
 function Header() {
   return (
     <header className="header">
-      <img className="header__logo" src={brainFlixLogo} alt="BrainFlix Logo" />
-      <form action="">
-        <input type="text" placeholder="Search" />
-      </form>
-      <div className="header__image-container--top"></div>
-      {/* <ProfileImageContainer /> */}
-      {/* <Image
-        src={muhanMuruge}
-        alt={`Head shot of Muhan
-       Muruge`}
-      /> */}
-      <button>Upload</button>
-      <div className="header__image-container--bottom"></div>
-      {/* <ProfileImageContainer /> */}
-      {/* <Image
-        src={muhanMuruge}
-        alt={`Head shot of Muhan
-       Muruge`}
-      /> */}
+      <div className="header__container">
+        <img
+          className="header__logo"
+          src={brainFlixLogo}
+          alt="BrainFlix Logo"
+        />
+        <div className="header__form-container">
+          <form className="header__form" action="">
+            <input
+              className="header__form-input"
+              type="text"
+              placeholder="Search"
+            />
+          </form>
+          <div className="header__image-container-top"></div>
+        </div>
+        <button className="header__button">UPLOAD</button>
+        <div className="header__image-container-bottom"></div>
+      </div>
     </header>
   );
 }
