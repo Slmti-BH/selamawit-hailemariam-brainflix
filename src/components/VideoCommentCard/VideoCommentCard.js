00c342c6f3e386d35a83e6680af9900b2c0df9ce
Selamawit Hailemariam <selamatwits@gmail.com>
2021-12-05 11:57:18 -0800

    Add styles for  mobile and tablet for the comment cards.

--- src/components/VideoCommentCard/VideoCommentCard.js
+++ src/components/VideoCommentCard/VideoCommentCard.js
@@ -1,15 +1,28 @@
+import "./VideoCommentCard.scss";
+import placeholderImage from "../../assets/images/images/image-placeholder.jpg";
+
 function VideoCommentCard(props) {
+   const date = new Date(props.timestamp);
+   const formattedDate = [
+     date.getDate(),
+     date.getMonth() + 1,
+     date.getUTCFullYear(),
+   ].join("/");
+
   return (
-    <div className="comment-card">
-      <img className="comment-card__image" src="" alt="" />
-      <div className="comment-card__wrapper">
-        <h3 className="comment-card__name">{props.name}</h3>
-        <time className="comment-card__date">{props.timestamp}</time>
+    <article className="comment-card">
+      <img className="comment-card__image" src={placeholderImage} alt="" />
+
+      <div className="comment-card__details">
+        <div className="comment-card__wrapper">
+          <h3 className="comment-card__name">{props.name}</h3>
+          <time className="comment-card__date">{formattedDate}</time>
+        </div>
+        <div>
+          <p className="comment-card__text">{props.commentContent}</p>
+        </div>
       </div>
-      <div>
-        <p className="comment-card__text">{props.commentContent}</p>
-      </div>
-    </div>
+    </article>
   );
 }
 
