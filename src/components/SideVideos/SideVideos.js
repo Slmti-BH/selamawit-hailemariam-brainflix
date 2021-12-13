024650462961d5d13f4a76df52159491ef9f2ef3
Selamawit Hailemariam <selamatwits@gmail.com>
2021-12-12 21:11:43 -0800

    Update the Link tags in side-videos.

--- src/components/SideVideos/SideVideos.js
+++ src/components/SideVideos/SideVideos.js
@@ -4,14 +4,15 @@ import VideoCard from "../VideoCard/VideoCard";
 import "./SideVideos.scss";
 
 function SideVideos(props) {
-  // console.log(props)
+
   return (
     <section className="side-videos">
       <h2 className="side-videos__title">NEXT VIDEOS</h2>
       <div className="side-videos__container">
         {props.videos.map((video) => {
           return (
-            <Link to={`/videos/${video.id}`} key={video.id}>
+            // use Link instead of onClick handlers
+            <Link className="side-videos__link" to={`/videos/${video.id}`} key={video.id}>
               <VideoCard
                 id={video.id}
                 src={video.image}
